9708f1184513d241d5e12744416ea3d49bc60282
James Watterson <jameslwatterson@gmail.com>
2017-04-11 17:04:29 -0700

    printed default suggestion

--- suggester.py
+++ suggester.py
@@ -1,1 +1,15 @@
-print('I suggest this place:')
+def print_suggestion(suggestion_dict):
+    print(suggestion_dict['name'])
+    print(suggestion_dict['category'])
+    print(suggestion_dict['street'])
+    print(suggestion_dict['city'])
+
+DEFAULT_SUGGESTION = {
+    'name': 'Mustang Donuts',
+    'category': 'Doughnuts',
+    'street': 'Hillcrest',
+    'city': 'Dallas'
+}
+
+print('I suggest this restaurant:')
+print_suggestion(DEFAULT_SUGGESTION)
